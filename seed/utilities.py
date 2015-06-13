fd14393349368859cf8360d14d4e0f67abd8b7d7
Adam Charnock <adam@omniwiki.co.uk>
2015-06-13 16:40:00 +0100

    Fixing occasional error encountered when running commands

--- seed/utilities.py
+++ seed/utilities.py
@@ -4,9 +4,9 @@ from seed.exceptions import ShellCommandError
 def run_command(command, ok_statuses=(0,)):
     p = Popen(command, shell=True, stdout=PIPE, stderr=STDOUT, close_fds=True)
     captured = p.stdout.read().decode("utf-8")
-    rc = p.poll()
+    rc = p.wait()
     
     if rc not in ok_statuses:
-        raise ShellCommandError("Command returned exit status %d: %s" % (rc, command), output=captured)
+        raise ShellCommandError("Command returned exit status %s: %s" % (rc, command), output=captured)
 
     return captured.strip()
