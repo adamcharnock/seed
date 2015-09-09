290bdb8772f2a463ee2fa2ba64e84e87ac63405f
Adam Charnock <adam@adamcharnock.com>
2015-09-09 03:36:52 +0100

    Version bump to 0.10.1 and updating CHANGES.txt

--- seed/__init__.py
+++ seed/__init__.py
@@ -1,4 +1,1 @@
-# It must be possible to import this file with 
-# none of the package's dependencies installed
 
-__version__ = '0.9.0'
--- setup.py
+++ setup.py
@@ -3,11 +3,10 @@
 from os.path import exists
 from setuptools import setup, find_packages
 
-from seed import __version__
 
 setup(
     name='seed',
-    version=__version__,
+    version=open('VERSION').read().strip(),
     author='Adam Charnock',
     author_email='adam@adamcharnock.com',
     packages=find_packages(),
